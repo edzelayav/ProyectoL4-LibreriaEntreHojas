ea073f315122a72c9ffe36eb455e39b793b16831
Ever <Ever@DESKTOP-N048FCK>
2022-02-16 10:52:14 -0600

    Guardando cambios tarea 3

--- EntreHojas.Web/EntreHojas.BL/ProductosBL.cs
+++ EntreHojas.Web/EntreHojas.BL/ProductosBL.cs
@@ -22,5 +22,30 @@ namespace EntreHojas.BL
             ListadeProductos = _contexto.Productos.ToList();
             return _contexto.Productos.ToList();
         }
+
+        public void GuardarProducto(Producto producto)
+        {
+            if(producto.Id == 0)
+            {
+                _contexto.Productos.Add(producto);
+            }
+            else
+            {
+                var productoExistente = _contexto.Productos.Find(producto.Id);
+                productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.Autor = producto.Autor;
+                productoExistente.Precio = producto.Precio;
+                productoExistente.Existencia = producto.Existencia;
+            }
+            
+            _contexto.SaveChanges();
+        }
+
+        public Producto ObtenerProducto(int id)
+        {
+            var producto = _contexto.Productos.Find(id);
+
+            return producto;
+        }
     }
 }
---
+++ EntreHojas.Web/EntreHojas.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,54 @@
+﻿using EntreHojas.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntreHojas.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosBL _productosBL;
+        public ProductosController()
+        {
+            _productosBL = new ProductosBL();
+        }
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var listadeProductos = _productosBL.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevoProducto = new Producto();
+
+            return View(nuevoProducto);
+        }
+
+       [HttpPost]
+        public ActionResult Crear(Producto producto)
+        {
+            _productosBL.GuardarProducto(producto);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+           var producto = _productosBL.ObtenerProducto(id);
+            return View(producto);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Producto producto)
+        {
+            _productosBL.GuardarProducto(producto);
+            
+            return RedirectToAction("Index");
+        }
+
+    }
+}
