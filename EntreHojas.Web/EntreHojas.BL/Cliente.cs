c34eb561945c4232df3ef40a113ec1f6f8cfd130
Ever <Ever@DESKTOP-N048FCK>
2022-03-17 17:52:41 -0600

    Cambios l4

---
+++ EntreHojas.Web/EntreHojas.BL/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntreHojas.BL
+{
+    public class Cliente
+    {
+      
+ 
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "Ingrese el nombre del cliente")]
+            [MinLength(2, ErrorMessage = "Ingrese mínimo 2 caracteres")]
+            public string Nombre { get; set; }
+
+            [Required(ErrorMessage = "Ingrese el teléfono")]
+            [MinLength(8, ErrorMessage = "El Numero de teléfono debe ser de 8 digitos")]
+            [MaxLength(8, ErrorMessage = "El Numero de teléfono debe ser de 8 digitos")]
+            public string Telefono { get; set; }
+
+            [Required(ErrorMessage = "Ingrese su dirección")]
+            [MinLength(6, ErrorMessage = "Ingrese mínimo 6 caracteres")]
+            public string Direccion { get; set; }
+            public bool Activo { get; set; }
+        }
+    }
+
+
+
+
+
+
+
--- EntreHojas.Web/EntreHojas.BL/Contexto.cs
+++ EntreHojas.Web/EntreHojas.BL/Contexto.cs
@@ -23,5 +23,15 @@ namespace EntreHojas.BL
 
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Categoria>Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenDetalle { get; set; }
+
+
+
+
+
+
     }
 }
